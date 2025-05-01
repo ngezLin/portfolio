1db6942d828f362ce183ad1e4e330bc7e9a2342f
ngezLin <89445359+ngezLin@users.noreply.github.com>
2025-05-01 20:53:12 +0700

    fix: &apos 2

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -52,7 +52,7 @@ const EmailSection = () => {
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           {" "}
           I&apos;m currently looking for new opportunities, my inbox is always
-          open. Whether you have a question or just want to say hi, I'll
+          open. Whether you have a question or just want to say hi, I&apos;ll
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
